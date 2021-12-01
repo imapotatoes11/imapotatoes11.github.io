20db81f30f586c724b673f4e6cf8eb03a2c82cd7
imapotatoes11 <79340132+imapotatoes11@users.noreply.github.com>
2021-11-30 22:18:29 -0500

    Add files via upload

---
+++ 2017/J1-quadrants.py
@@ -0,0 +1,7 @@
+coordX=int(input())
+coordY=int(input())
+if coordX>0:
+    if coordY>0:
+        print("1")
+    if coordY<0:
+        print("4")
---
+++ 2017/J2-ShiftySum.py
@@ -0,0 +1,6 @@
+N=int(input())
+k=int(input())
+times=1
+for i in range(k):
+    times=times*10+1
+print(N*times)
