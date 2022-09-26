4270dca5f3560210aa0a47c7a7607f04974de1fc
imapotatoes11 <79340132+imapotatoes11@users.noreply.github.com>
2022-09-26 14:12:43 -0400

    minor changes

--- testes3.py
+++
@@ -1,6 +0,0 @@
-total2=0
-j=1
-while j<5:
-    total2+=j
-    j+=1
-print(total2)
--- testes5.py
+++
@@ -1,9 +0,0 @@
-e={
-    "george":12,
-    "bob":14,
-    "barthelomew":13,
-}
-print(e["george"])
-print(e)
-e["hello"]=2
-print(e)
