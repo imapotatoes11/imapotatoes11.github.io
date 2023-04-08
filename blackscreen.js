2ea0d8578524e0e16de96745758fe8d5a72a32a1
imapotatoes11 <79340132+imapotatoes11@users.noreply.github.com>
2023-04-08 19:30:53 -0400

    minor changes

--- blackscreen.js
+++ blackscreen.js
@@ -1,6 +1,5 @@
 // change the document background color to black when an element with class "black" is in viewport
-
-document.addEventListener('scroll', ()=>{
+function checkLDM(){
     const blackscreen = document.getElementsByClassName("black");
     const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
@@ -8,4 +7,8 @@ document.addEventListener('scroll', ()=>{
             else {document.body.style.backgroundColor = 'white';}});
     });
     for (let i=0;i<blackscreen.length;i+=1){observer.observe(blackscreen[i]);}
-});
+}
+
+
+document.addEventListener('scroll', checkLDM);
+document.addEventListener('DOMContentLoaded', checkLDM)
