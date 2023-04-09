0ca118f1bbdfa67c964160027d4cf09c52e471e0
imapotatoes11 <79340132+imapotatoes11@users.noreply.github.com>
2023-04-09 12:38:14 -0400

    minoir changes

--- blackscreen.js
+++ blackscreen.js
@@ -3,8 +3,14 @@ function checkLDM(){
     const blackscreen = document.getElementsByClassName("black");
     const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
-            if (entry.isIntersecting) {document.body.style.backgroundColor = 'black';}
-            else {document.body.style.backgroundColor = 'white';}});
+            if (entry.isIntersecting) {
+                // executed if black element in viewport
+                document.body.style.backgroundColor = 'black';
+            }
+            else {
+                // executed if black element not in viewport
+                document.body.style.backgroundColor = 'white';
+            }});
     });
     for (let i=0;i<blackscreen.length;i+=1){observer.observe(blackscreen[i]);}
 }
